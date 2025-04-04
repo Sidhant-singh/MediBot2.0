16dff805d3a975f47fa0380043c8254497aba9e5
Sidhant singh <ss1807079@gmail.com>
2025-04-04 17:37:44 +0530

    Updated code for medibot2.0

---
+++ brain_of_the_doctor.py
@@ -0,0 +1,50 @@
+#Step 1 : Setup Groq API key
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+
+#Step 2 : Convert image to required format
+import base64
+image_path = "acne.jpg"
+image_file = open(image_path, "rb")
+encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
+# file being decoded to utf8 and encoded to base64
+
+
+def encode_image(image_path):
+    image_file = open(image_path, "rb")
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+
+#step 3 : Setup multimodal LLM
+from groq import Groq
+query = "Is there something wrong with my face"
+model = "llama-3.2-90b-vision-preview"
+def analyze_image_with_query(query,model,encoded_image):
+    client = Groq()
+
+    messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text", 
+                        "text": query
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{encoded_image}",
+                        },
+                    },
+                ],
+            }]
+    chat_completion=client.chat.completions.create(
+        messages=messages,
+        model=model
+    )
+    return chat_completion.choices[0].message.content
+
+
---
+++ voice_of_the_doctor.py
@@ -0,0 +1,87 @@
+# Step 1a : Setup Text to speech-TTS-model for voice generation with gTTs
+import os
+from gtts import gTTS
+
+def text_to_speech_with_gtts_old(input_text,output_filepath):
+    language = 'en'
+
+    audioobj = gTTS(text=input_text, lang=language, slow=False)
+
+    audioobj.save(output_filepath)
+
+input_text = "Hello, I am the voice of the doctor. How can I assist you today?"
+# text_to_speech_with_gtts_old(input_text=input_text,output_filepath="gtts.testing.mp3")
+
+# Step 1b : Setup Text to speech-TTS-model for voice generation with ElevenLabs
+import elevenlabs
+from elevenlabs.client import ElevenLabs
+from dotenv import load_dotenv
+load_dotenv()
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
+def text_to_speech_with_elevenlabs_old(input_text,output_filepath):
+    client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
+    audio = client.generate(
+        text=input_text,
+        voice="Aria",
+        model="eleven_turbo_v2",
+        output_format="mp3_22050_32"
+    )
+    elevenlabs.save(audio, output_filepath)
+
+# text_to_speech_with_elevenlabs_old(input_text=input_text,output_filepath="elevenlabs.testing.mp3")
+    # Save the audio to a file
+
+
+# Step 2 : Use model for text output to voice
+# for auto playing the audio we use subprocess library to play the audio file
+import subprocess
+import platform
+from gtts import gTTS
+def text_to_speech_with_gtts(input_text,output_filepath):
+    language = 'en'
+
+    audioobj = gTTS(text=input_text, lang=language, slow=False)
+
+    audioobj.save(output_filepath)
+    os_name = platform.system()
+    try:
+        if os_name == "Darwin":  # macOS
+            subprocess.run(['afplay', output_filepath])
+        elif os_name == "Windows":  # Windows
+            subprocess.run(['start', output_filepath],shell=True)
+        elif os_name == "Linux":  # Linux
+            subprocess.run(['mpg123', output_filepath])  # Alternative: use 'mpg123' or 'ffplay'
+        else:
+            raise OSError("Unsupported operating system")
+    except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")
+
+input_text = "Hello, I am sidhant singh bhadauriya. How can I assist you today?"
+# text_to_speech_with_gtts(input_text=input_text,output_filepath="gtts_testing_autoplaying.mp3")
+
+
+
+def text_to_speech_with_elevenlabs(input_text,output_filepath):
+    client = ElevenLabs(api_key=ELEVENLABS_API_KEY)
+    audio = client.generate(
+        text=input_text,
+        voice="Aria",
+        model="eleven_turbo_v2",
+        output_format="mp3_22050_32"
+    )
+    elevenlabs.save(audio, output_filepath)
+    os_name = platform.system()
+    try:
+        if os_name == "Darwin":  # macOS
+            subprocess.run(['afplay', output_filepath])
+        elif os_name == "Windows":  # Windows
+            subprocess.run(['start', output_filepath],shell=True)
+            # subprocess.run(['powershell', '-c', f'(New-Object Media.SoundPlayer "{output_filepath}").PlaySync();'])
+        elif os_name == "Linux":  # Linux
+            subprocess.run(['aplay', output_filepath])  # Alternative: use 'mpg123' or 'ffplay'
+        else:
+            raise OSError("Unsupported operating system")
+    except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")
+
+text_to_speech_with_elevenlabs(input_text=input_text,output_filepath="elevenlabs_testing_autoplaying.mp3")
